22abc4261b42770f8a227229681128a2b2072694
ChanMyaeAung <chanmyae.cma30@gmail.com>
2022-11-16 16:09:30 +0630

    dynamic component border color

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="project">
+  <div class="project" :class="{complete:project.complete}">
     <div class="flexing">
         <div>
             <h3 @click="showDetail=!showDetail">{{project.title}}</h3>
@@ -11,6 +11,7 @@
         </div>
     </div>
     <p v-if="showDetail">{{project.detail}}</p>
+    {{project.complete}}
   </div>
 </template>
 
@@ -61,5 +62,8 @@ span{
 }
 span:hover{
     color:#777;
+}
+.complete{
+    border-left-color: green;
 }
 </style>
